ee2e971478dbfff7cc9e1bef5a4bfbd16f38dc2d
2jeongmin <n2atnut@uos.ac.kr>
2021-12-21 18:22:10 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/shm_bb_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shm_bb_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new WidgetDemo());
+
+class WidgetDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => WidgetDemoState();
+}
+
+class WidgetDemoState extends State<WidgetDemo> {
+  int _curIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo App',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('SHM_BB Demo'),
+        ),
+        body: getPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _curIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                size: 30,
+                color: _curIndex == 0 ? Colors.blue : Colors.black54,
+              ),
+              title: Text(
+                "Settings",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: _curIndex == 0 ? Colors.blue : Colors.black54),
+              ),
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Colors.blue,
+              icon: Icon(
+                Icons.visibility,
+                size: 30,
+                color: _curIndex == 1 ? Colors.blue : Colors.black54,
+              ),
+              title: Text(
+                "Detection",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: _curIndex == 1 ? Colors.blue : Colors.black54),
+              ),
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Colors.blue,
+              icon: Icon(
+                Icons.settings_ethernet,
+                size: 30,
+                color: _curIndex == 2 ? Colors.blue : Colors.black54,
+              ),
+              title: Text(
+                "Measurement",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: _curIndex == 2 ? Colors.blue : Colors.black54),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget? getPage() {
+    Widget? page;
+    switch (_curIndex) {
+      case 0:
+        page = page1();
+        break;
+      case 1:
+        page = page2();
+        break;
+      case 2:
+        page = page3();
+        break;
+    }
+    return page;
+  }
+
+  Container page1() {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Settings',
+        style: TextStyle(color: Colors.blue, fontSize: 30),
+      ),
+    );
+  }
+
+  GridView page2() {
+    return GridView.count(
+      padding: const EdgeInsets.all(10),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 50,
+      crossAxisCount: 3,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text("pic 1"),
+          color: Colors.blue[100],
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text('pic 2'),
+          color: Colors.blue[200],
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text('pic 3'),
+          color: Colors.blue[300],
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text('pic 4'),
+          color: Colors.blue[400],
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text('pic 5'),
+          color: Colors.blue[500],
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: const Text('pic 6'),
+          color: Colors.blue[600],
+        ),
+      ],
+    );
+  }
+
+  Container page3() {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Displacement Measurement',
+        style: TextStyle(color: Colors.blue, fontSize: 20),
+      ),
+    );
+  }
+}
