32af24c25c7ae5a7976796afda91c2c4831b6a71
2jeongmin <n2atnut@uos.ac.kr>
2021-12-23 13:54:09 +0900

    Update main.dart
    gallery navigator.push

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,80 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new WidgetDemo());
 
+class WidgetDem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo App',
+      home: Gallery(),
+    );
+  }
+}
+
+class Gallery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: page4());
+  }
+}
+GridView page4() {
+  return GridView.count(
+    padding: const EdgeInsets.all(10),
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 50,
+    crossAxisCount: 3,
+    children: <Widget>[
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text("pic 1"),
+        color: Colors.blue[100],
+      ),
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text('pic 2'),
+        color: Colors.blue[200],
+      ),
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text('pic 3'),
+        color: Colors.blue[300],
+      ),
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text('pic 4'),
+        color: Colors.blue[400],
+      ),
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text('pic 5'),
+        color: Colors.blue[500],
+      ),
+      Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: const Text('pic 6'),
+        color: Colors.blue[600],
+      ),
+      // ElevatedButton(
+      //     child: Text('Go back'),
+      //     onPressed: () {
+      //       Navigator.push(
+      //         context,
+      //         MaterialPageRoute(builder: (context) => new Scaffold()),);})
+    ],
+  );
+}
+
+
 class WidgetDemo extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => WidgetDemoState();
@@ -98,51 +172,19 @@ class WidgetDemoState extends State<WidgetDemo> {
       ),
     );
   }
-
-  GridView page2() {
-    return GridView.count(
-      padding: const EdgeInsets.all(10),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 50,
-      crossAxisCount: 3,
-      children: <Widget>[
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text("pic 1"),
-          color: Colors.blue[100],
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text('pic 2'),
-          color: Colors.blue[200],
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text('pic 3'),
-          color: Colors.blue[300],
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text('pic 4'),
-          color: Colors.blue[400],
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text('pic 5'),
-          color: Colors.blue[500],
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8),
-          child: const Text('pic 6'),
-          color: Colors.blue[600],
-        ),
-      ],
+  Container page2() {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      child: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Gallery()),
+          );
+        },
+        child: Icon(Icons.add_a_photo),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 
@@ -156,3 +198,4 @@ class WidgetDemoState extends State<WidgetDemo> {
     );
   }
 }
+
